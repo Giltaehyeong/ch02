7b5a63ea5210bd9717af32c0df97041f76298a85
Giltaehyeong <r1605866@naver.com>
2022-05-19 22:58:54 +0900

    0518. 관심사의 분리와 MVC패턴 - 원리(2) * 학습목표 스프링 MVC 패턴이 어떻게 동작하는지 원리에 대해서 알아보자.
    1. MethodCall2.java 프로그램 작성
     - 1. YoilTellerMVC의 객체를 생성
    Class clazz = Class.forName("com.fastcampus.ch2.YoilTellerMVC");
    Object obj = clazz.newInstance();
    
     - 2. main메서드의 정보를 가져온다.
    Method main = clazz.getDeclaredMethod("main", int.class, int.class,
    int.class, Model.class);
    
     - 3. Model을 생성
    Model model = new BindingAwareModelMap();
    System.out.println("[before] model="+model);
    
     - 4. main메서드를 호출
    String viewName = (String)main.invoke(obj, new Object[] { 2021, 10, 1,
    model }); // Reflection API를 이용한 호출
    System.out.println("viewName="+viewName);
    
    2. MethodCall3.java 프로그램 작성
     - 2번 3번 예제 둘다 spring이 어떻게 매개변수에 알아서 값들을 넣어주고 변환하는지
       원리를 간단한 프로그램으로 만들어본것임.
    
    3. MyDispatcherServlet.java
     - MethodCall3.java를 원격 프로그램으로 만든 예제
    @WebServlet 오류해결
     - 톰캣 라이브러리 추가하기.

--- src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
+++ src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
@@ -1,59 +1,46 @@
 package com.fastcampus.ch2;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Calendar;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 
-// 년원일을 입력하면 요일을 알려주는 프로그램
 @Controller
-public class YoilTellerMVC { // http://localhost/ch2/getYoilMVC?year=2021&month=10&day=1
-		@RequestMapping("/getYoilMVC")
-//		public void main(HttpServletRequest request, HttpServletResponse response) throws IOException{
-		public String main(int year, int month, int day, Model m) throws IOException{
-//			  뷰 이름을 반환해야되서 void -> String 으로
-		
-//			ModelAndView mv = new ModelAndView();	
-			
-			//1. 유효성 검사 : 값이 들어왔는지 안들어왔는지 확인.
-			if(!isValid(year, month, day))
-				return "yoilError";
-			
-			//2. 요일 받아오는 부분을 별도의 메서드로 뽑는다.
-			// 우클릭 -> Refactor -> Extract Method(코드를 별도의 메서드로 뽑아냄.)
-			char yoil = getYoil(year, month, day);
-			
-			//3. 계산한 결과를 model에 저장
-			m.addAttribute("year", year);
-			m.addAttribute("month", month);
-			m.addAttribute("day", day);
-			m.addAttribute("yoil", yoil);
-			
-//			//4. 결과를 보여줄 View를 지정
-//			mv.setViewName("yoil");
-//			return mv;
-			
-			return "yoil"; // /WEB-INF/views/yoil.jsp
+public class YoilTellerMVC {
+    @RequestMapping("/getYoilMVC") // http://localhost/ch2/getYoilMVC
+    public String main(int year, int month, int day, Model model) {
+ 
+        // 1. 유효성 검사
+    	if(!isValid(year, month, day)) 
+    		return "yoilError";  // 유효하지 않으면, /WEB-INF/views/yoilError.jsp로 이동
+    	
+        // 2. 처리
+    	char yoil = getYoil(year, month, day);
 
-		}
-		
-		//메서드의 접근자를 private로 한 이유는 이 class 안에서만 사용하기 떄문.
-		private boolean isValid(int year, int month, int day) {
-			// TODO Auto-generated method stub
-			return true;
-		}
+        // 3. Model에 작업 결과 저장
+        model.addAttribute("year", year);
+        model.addAttribute("month", month);
+        model.addAttribute("day", day);
+        model.addAttribute("yoil", yoil);
+        
+        // 4. 작업 결과를 보여줄 View의 이름을 반환
+        return "yoil"; // /WEB-INF/views/yoil.jsp
+    }
+    
+    private char getYoil(int year, int month, int day) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month - 1, day);
 
-		private char getYoil(int year, int month, int day) {
-			Calendar cal = Calendar.getInstance();
-			cal.set(year, month - 1, day);
-			
-			int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK); // 1:일요일, 2:월요일 ...
-			return " 일월화수목금토".charAt(dayOfWeek);
-		}
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+        return " 일월화수목금토".charAt(dayOfWeek);
+    }
+    
+    private boolean isValid(int year, int month, int day) {    
+    	if(year==-1 || month==-1 || day==-1) 
+    		return false;
+    	
+    	return (1<=month && month<=12) && (1<=day && day<=31); // 간단히 체크 
+    }
 }
