95d038dc996692f730ef2eb609255af98567d071
Giltaehyeong <r1605866@naver.com>
2022-05-19 18:33:07 +0900

    0518. 관심사의 분리와 MVC패턴 - 원리(1) * 학습목표 스프링 MVC 패턴이 어떻게 동작하는지 원리에 대해서 알아보자.
    1. MethodInfo.java
     - YoilTeller 클래스의 객체를 생성
    Class clazz = Class.forName("com.fastcampus.ch2.YoilTeller");
    Object obj = clazz.newInstance();
    
     - 모든 메서드 정보를 가져와서 배열에 저장(메서드가 n개일 수 있어 배열로 선언)
    Method[] methodArr = clazz.getDeclaredMethods();
    
     - 반복문으로 메서드 이름, 매개변수 목록, 메서드의 반환타입 출력.
     - StringJoiner paramList = new StringJoiner(", ", "(", ")"); 구분자 접두사
    접미사
     - 반복문에
    paramList.add(paramType.getName() + " " + paramName); 적어주면
    void main(javax.servlet.http.HttpServletRequest arg0,
    javax.servlet.http.HttpServletResponse arg1) 형태로 출력.
                                             매개변수 타입  이름           매개변수 타입
    이름
     - 매개변수 이름이
     - request, response가 아닌 이유는 컴파일러한테 매개변수 타입은 중요하지만
     - 매개변수 이름은 중요하지 않다.
     - 그래서 매개변수 이름을 저장할라면 -parameters 옵션을 추가해야함.
     - 어디에? javac(자바 컴파일러)에
     - Preferences - Java - compiler - Store inform.... 체크 - pom.xml 가서 -
    java-version 11로 변경.
     - pom.xml 파일을 수정하게 되면 update 해줘야함 꼭 !!
    void main(javax.servlet.http.HttpServletRequest request,
    javax.servlet.http.HttpServletResponse response)
                                           매개변수  타입   이름
     - 정확하게 나옴.
    
    * 매개변수 이름을 얻는 방법 2가지
    1. Reflection API 사용 -parameters (JDK 1.8 이후버전으로)
    2. Class file을 직접 읽는 방법
    
    2. MethodCall.java
    - 참조 매개변수 값 받아오는 실습 프로그램
    - main 메서드에서 생성한 map에
    - mc.main 메서드의 model 값들을
    - main 메서드에서 생성한 map 저장소에
    - 담아서 뷰이름과 반환한다.
    - 뷰의 내용을 한줄씩 읽어서 하나의 문자열로 만들고
    - map 담긴 key를 하나씩 읽어서 template의 ${key}를 value로 바꾼다.
    - 렌더링 결과를 반환한다.
    - txtView.txt 파일에 있는 EL 출력.

--- src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
+++ src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
@@ -16,30 +16,30 @@ import org.springframework.web.servlet.ModelAndView;
 public class YoilTellerMVC { // http://localhost/ch2/getYoilMVC?year=2021&month=10&day=1
 		@RequestMapping("/getYoilMVC")
 //		public void main(HttpServletRequest request, HttpServletResponse response) throws IOException{
-		public ModelAndView main(int year, int month, int day) throws IOException{
+		public String main(int year, int month, int day, Model m) throws IOException{
 //			  뷰 이름을 반환해야되서 void -> String 으로
 		
-			ModelAndView mv = new ModelAndView();	
+//			ModelAndView mv = new ModelAndView();	
 			
 			//1. 유효성 검사 : 값이 들어왔는지 안들어왔는지 확인.
-//			if(!isValid(year, month, day))
-//				return "yoilError";
+			if(!isValid(year, month, day))
+				return "yoilError";
 			
 			//2. 요일 받아오는 부분을 별도의 메서드로 뽑는다.
 			// 우클릭 -> Refactor -> Extract Method(코드를 별도의 메서드로 뽑아냄.)
 			char yoil = getYoil(year, month, day);
 			
 			//3. 계산한 결과를 model에 저장
-			mv.addObject("year", year);
-			mv.addObject("month", month);
-			mv.addObject("day", day);
-			mv.addObject("yoil", yoil);
+			m.addAttribute("year", year);
+			m.addAttribute("month", month);
+			m.addAttribute("day", day);
+			m.addAttribute("yoil", yoil);
 			
-			//4. 결과를 보여줄 View를 지정
-			mv.setViewName("yoil");
-			return mv;
+//			//4. 결과를 보여줄 View를 지정
+//			mv.setViewName("yoil");
+//			return mv;
 			
-//			return "yoil"; // /WEB-INF/views/yoil.jsp
+			return "yoil"; // /WEB-INF/views/yoil.jsp
 
 		}
 		
