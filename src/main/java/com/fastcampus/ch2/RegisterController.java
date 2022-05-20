b7967a9ec413eb1733b0801594518d3e5f28b72c
Giltaehyeong <r1605866@naver.com>
2022-05-20 22:38:52 +0900

    0520. @GetMapping, @PostMapping(1) 1. Register.java 컨트롤러 @RequestMapping 수정 @GetMapping("register/add") // 신규회원 가입 화면 @PostMapping("/register/save") //4.3 부터 적용
    컨트롤러 유효성 검사
    // 1. 유효성 검사
    	if(!isValid(user)) {
    		String msg = URLEncoder.encode("id를 잘못입력하셨습니다.", "utf-8");
    
    		m.addAttribute("msg", msg); //model에 담아서 넘겨줄 수 있음.
    		return "redirect:/register/add"; //URL재작성(rewriting)
    }
    
    *GET view 페이지 -> servlet-context에 view 컨트롤러 선언
    <view-controller path="register/add" view-name="registerForm" />
    
    버전변경
    <org.springframework-version>5.0.7.RELEASE</org.springframework-version>

---
+++ src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -0,0 +1,40 @@
+package com.fastcampus.ch2;
+
+import java.net.URLEncoder;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller //ctrl + shift + o
+public class RegisterController {
+	
+//	@RequestMapping(value = "register/add", method={RequestMethod.GET, method=RequestMethod.POST})
+														//둘다 허용해준다.
+//	<view-controller path="register/add" view-name="registerForm" />
+//	@GetMapping("register/add") // 신규회원 가입 화면
+//	public String register() {
+//		return "registerForm";
+//	}
+	
+//	@RequestMapping(value = "register/save", method=RequestMethod.POST)
+//	 Request method 'GET' not supported
+	@PostMapping("/register/save") //4.3 부터 적용
+	public String save(User user, Model m) throws Exception {
+		// 1. 유효성 검사
+		if(!isValid(user)) {
+			String msg = URLEncoder.encode("id를 잘못입력하셨습니다.", "utf-8");
+			
+			m.addAttribute("msg", msg); //model에 담아서 넘겨줄 수 있음.
+			return "redirect:/register/add"; //URL재작성(rewriting)
+//			return "redirect:/register/add?msg="+msg; //URL재작성(rewriting)
+		}
+		
+		// 2. DB에 신규회원 정보를 저장
+		return "registerInfo";
+	}
+
+	private boolean isValid(User user) {
+		return true;
+	}
+}
