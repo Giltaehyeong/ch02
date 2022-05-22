98a3d540aae349855f45fed8455d240efaf2c40a
Giltaehyeong <r1605866@naver.com>
2022-05-22 20:39:52 +0900

    0522. 세션(Session) - 실습(1) 1. BoardList.jsp 보드 페이지, BoardController.java 만들기  - 보드 버튼을 클릭하면  - 로그인이 되어있는지 여부를 확인하고  - boardList.jsp 페이지를 불러오도록 구현  - loginCheck 메서드 // 1. 세션을 얻어서  HttpSession session = request.getSession(); // 2. 세션에 id가 있는지 확인, 있으면 true를 반환  return session.getAttribute("id")!=null;  - 되어있지 않다면 로그인 페이지로 redirect.  if(!loginCheck(request))    return "redirect:/login/login"; // 로그인을 안했으면 로그인 화면으로 이동
    2. 로그인/로그아웃 기능 만들기
     - 로그인컨트롤러에..
     - 로그인
      // 2-2. id와 pwd가 일치하면, 홈으로 이동
      // 세션 객체를 얻어오기
      HttpSession session = request.getSession();
      // 세션 객체에 id를 저장
      session.setAttribute("id", id);
     - 로그인 되면 홈으로 이동.
    
     - 로그아웃
     @GetMapping("/logout")
     public String logout(HttpSession session) {
     // 1. 세션을 종료
     session.invalidate(); // 세션을 소멸시키는 메서드
     // 2. 홈으로 이동
     return "redirect:/";
     }
    
    3. index.jsp 페이지에서 세션ID가 있는지 확인하기 위해
    - 상단에서 3항 연산자 사용
    <c:set var="loginOutLink" value="${sessionScope.id==null ?
    '/login/login' : 'login/logout'}"/>
     - 로그인 안한상태면 로그인 페이지로 : 로그인 상태이면 로그인 로그아웃 출력
    <c:set var="loginOut" value="${sessionScope.id==null ? 'Login' :
    'Logout'}"/>
    - view단에서 출력.
    <li><a href="<c:url value='${loginOutLink}'/>">${loginOut}</a></li>
    
    4. BoardList.jsp 페이지 3번 동일작업 완료

--- src/main/java/com/fastcampus/ch2/LoginController.java
+++ src/main/java/com/fastcampus/ch2/LoginController.java
@@ -3,7 +3,9 @@ package com.fastcampus.ch2;
 import java.net.URLEncoder;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +20,17 @@ public class LoginController {
 		return "loginForm";
 	}
 	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		// 1. 세션을 종료
+		session.invalidate();
+		// 2. 홈으로 이동
+		return "redirect:/";
+	}
+	
 	@PostMapping("/login")
-	public String login(String id, String pwd, boolean rememberId, HttpServletResponse response) throws Exception{
+	public String login(String id, String pwd, boolean rememberId, 
+			HttpServletRequest request, HttpServletResponse response) throws Exception{
 		System.out.println("id="+id);
 		System.out.println("pwd="+id);
 		System.out.println("rememberId="+rememberId);
@@ -32,6 +43,11 @@ public class LoginController {
 		}
 		
 		// 2-2. id와 pwd가 일치하면, 홈으로 이동
+		// 세션 객체를 얻어오기
+		HttpSession session = request.getSession();
+		// 세션 객체에 id를 저장
+		session.setAttribute("id", id);
+		
 		
 		if(rememberId) {
 			//		1. 쿠키를생성
