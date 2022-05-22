09efe7fdd17276dcb488f2660efe567f21f104d0
Giltaehyeong <r1605866@naver.com>
2022-05-22 13:15:51 +0900

    0522. redirect와 forward 1. redirect  - 2번의 요청
    2. forward
     - 1번의 요청
     - GET 방식
     - URL 변화 x 왜? 바로 전달하니깐

--- src/main/java/com/fastcampus/ch2/RegisterController.java
+++ src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -5,17 +5,19 @@ import java.net.URLEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller //ctrl + shift + o
 public class RegisterController {
 	
-//	@RequestMapping(value = "register/add", method={RequestMethod.GET, method=RequestMethod.POST})
+	@RequestMapping(value = "/register/add", method = {RequestMethod.GET, RequestMethod.POST})
 														//둘다 허용해준다.
 //	<view-controller path="register/add" view-name="registerForm" />
 //	@GetMapping("register/add") // 신규회원 가입 화면
-//	public String register() {
-//		return "registerForm";
-//	}
+	public String register() {
+		return "registerForm";
+	}
 	
 //	@RequestMapping(value = "register/save", method=RequestMethod.POST)
 //	 Request method 'GET' not supported
@@ -26,7 +28,7 @@ public class RegisterController {
 			String msg = URLEncoder.encode("id를 잘못입력하셨습니다.", "utf-8");
 			
 			m.addAttribute("msg", msg); //model에 담아서 넘겨줄 수 있음.
-			return "redirect:/register/add"; //URL재작성(rewriting)
+			return "forward:/register/add"; //URL재작성(rewriting)
 //			return "redirect:/register/add?msg="+msg; //URL재작성(rewriting)
 		}
 		
@@ -35,6 +37,6 @@ public class RegisterController {
 	}
 
 	private boolean isValid(User user) {
-		return true;
+		return false;
 	}
 }
