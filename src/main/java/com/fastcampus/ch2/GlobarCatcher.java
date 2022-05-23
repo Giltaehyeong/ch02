f978b16389928cae064818ec29c03da6fa96a137
Giltaehyeong <r1605866@naver.com>
2022-05-23 14:48:11 +0900

    0523. 예외처리 1. error.jsp  - 예외처리를 출력해주는 페이지 발생한 예외 : ${ex}<br> 예외 메시지 : ${ex.message}<br>  - 예외 메세지를 출력해주기 위해 컨트롤러에서 model 객체에 담아 전송해야됨.
    2. ExceptionController.java, ExceptionController2.java
     - 예외처리를 해주는 컨트롤러
    NullPointerException? -> Null값으로 선언된 개체를 참조를 사용하려고 하면 발생
    FileNotFoundException? -> 호출된 파일을 찾을 수 없을 때 발생,,
    BindingResult? -> binding 또는 검증과정에서 오류를 발견하면 그에 대한 정보를 등록할 수 있는 메서드를
    제공한다.
    binding?
    -> 실제 함수가 위치한 메모리를 연결하는것, 프로그램에 사용된 구성 요소의 실제 값 또는 프로퍼티를 결정짓는 행위를 의미.
    - 예외처리는 제일 가까운곳에서 선언되어있는 예외처리 메서드에서 처리한다.
    
    3. GlobarCatcher.java
    @ControllerAdvice("com.fastcampus.ch3") //지정된 패키지에서 발생한 예외처리
    //@ControllerAdvice 모든패키지 적용
    // @ControllerAdvice 선언하면 모든 컨트롤러에서 발생하는 예외들을 여기있는 메서드들이 처리함.

---
+++ src/main/java/com/fastcampus/ch2/GlobarCatcher.java
@@ -0,0 +1,26 @@
+package com.fastcampus.ch2;
+
+import java.io.FileNotFoundException;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice("com.fastcampus.ch3") //지정된 패키지에서 발생한 예외처리
+//@ControllerAdvice 모든패키지 적용
+// @ControllerAdvice 선언하면 모든 컨트롤러에서 발생하는 예외들을 여기있는 메서드들이 처리함.
+public class GlobarCatcher {
+
+	@ExceptionHandler({NullPointerException.class, FileNotFoundException.class})
+	public String catcher2(Exception ex, Model m) {
+		m.addAttribute("ex", ex);
+		return "error";
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public String catcher(Exception ex, Model m) {
+		m.addAttribute("ex", ex);
+		return "error";
+	}
+	
+}
