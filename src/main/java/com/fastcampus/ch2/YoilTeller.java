927e669f7de8ed4f00595f8f66f1cb3f033ba10d
seulgae <r1605866@naver.com>
2022-06-23 00:07:31 +0900

    0622. Spring MVC ch02 작업내용 1. RequestHeader.java - 요청 메세지에 있는 헤더 정보 출력해주는 프로그램
    2. RequestMessage.java
    - GET요청과 POST요청의 구조 차이
    
    관심사의 분리, MVC 패턴
    1. 입력의 분리(공통 코드 떼어냄)M
    2. 처리(모델을 통한 결과 저장)C
    3. 출력의 분리(변하는것과 변하지 않는 것의 분리)V

--- src/main/java/com/fastcampus/ch2/YoilTeller.java
+++ src/main/java/com/fastcampus/ch2/YoilTeller.java
@@ -17,12 +17,23 @@ public class YoilTeller { // http://localhost:8080/getYoil?year=2021&month=10&da
 	@RequestMapping("/getYoil")
 	public void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		 							//문자열 배열 자동으로 만들어줌.
+		
+		//관심사 Concerns -> 해야할 작업
+		//OOP 5대 설계원칙 - SOLID
+		//SRP : 단일책임의 원칙(하나의 메서드는 하나의 책임)
+		
+		//관심사 분리에는 3가지 원칙이 있다.
+		//1. 관심사
+		//2. 변하는것, 변하지 않는 것
+		//  common, uncommon
+		//3. 공통(중복)코드
+		
 		// 1. 입력
 		String year = request.getParameter("year");
 		String month = request.getParameter("month");
 		String day = request.getParameter("day");
 		
-		//2. 작업
+		//2. 작업(요일계산)
 		int yyyy = Integer.parseInt(year);
 					//문자열을 정수값으로 변환하여 받아옴.
 		int mm = Integer.parseInt(month);
@@ -39,6 +50,6 @@ public class YoilTeller { // http://localhost:8080/getYoil?year=2021&month=10&da
 		response.setCharacterEncoding("utf-8");
 		PrintWriter out = response.getWriter(); // response 객체에서 브라우저로의 출력 스트림을 얻는다.
 		out.println(year + "년 " + month + "월 " + day + "일은 ");
-		out.println(yoil + "요일입니다.");	
+		out.println(yoil + "요일입니다.");
 	}
 }
